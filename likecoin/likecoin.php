557e370e027d76849f6b510584178ed5aa8c1462
William Cheung <wlamc@oursky.com>
2018-07-06 14:33:13 +0800

    ✨ Add init, upgrade & uninstall handler

--- likecoin/likecoin.php
+++ likecoin/likecoin.php
@@ -34,3 +34,37 @@ function likecoin_register_meta_boxes() {
 
 add_action( 'admin_enqueue_scripts', 'likecoin_load_scripts' );
 add_action( 'add_meta_boxes', 'likecoin_register_meta_boxes' );
+
+/* Init / Upgrade related */
+
+function handle_init_and_upgrade() {
+  global $wpdb;
+  global $charset_collate;
+  $version = get_option( 'likecoin_plugin_version', '0.1' );
+  $table_name = $wpdb->prefix . 'likecoin_author';
+  $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+    `author_id` int NOT NULL,
+    `likecoin_id` varchar(255) NOT NULL,
+    PRIMARY KEY (`author_id`)
+  ) $charset_collate;";
+  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+  dbDelta( $sql );
+
+  if ( version_compare( $version, '1.0' ) < 0 ) {
+    update_option( 'likecoin_plugin_version', '0.1' );
+  }
+}
+
+function handle_uninstall() {
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'likecoin_author';
+  $sql = "DROP TABLE IF NOT EXISTS $table_name;";
+  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+  dbDelta( $sql );
+  delete_option( 'likecoin_plugin_version' );
+}
+
+register_activation_hook( __FILE__, 'handle_init_and_upgrade' );
+add_action( 'upgrader_process_complete', 'handle_init_and_upgrade' );
+add_action( 'init', 'handle_init_and_upgrade' );
+register_uninstall_hook( __FILE__, 'handle_uninstall' );
