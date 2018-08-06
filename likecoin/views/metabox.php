6e5fb2af518d94a592eef15bef8e4633d82f9e85
William Chong <williamchong007@msn.com>
2018-08-06 18:06:55 +0800

    💄 Style options metabox according to design

--- likecoin/views/metabox.php
+++ likecoin/views/metabox.php
@@ -23,52 +23,63 @@ function likecoin_add_meta_box( $post ) {
 	<h3>Please unlock your wallet</h3>
 	</div>
 	<div class="likecoin webThreeError needLikeCoinId" style="display: none">
-	<h3>Need Like Coin Id</h3>
+	<h3><a href="https://like.co/in/register" target="_blank">Please register a LikeCoin Id first</a></h3>
 	</div>
 	<div class="likecoin webThreeError needLogin" style="display: none">
-	<h3><a class="loginBtn" style="cursor: pointer">Login to get Like Coin Id</a></h3>
+	<h3><a class="loginBtn" style="cursor: pointer">Login to get LikeCoin Id</a></h3>
 	</div>
 </section>
 <section class="likecoin optionsSection" style="<?php echo $has_likecoin_id ? '' : 'display: none'; ?>">
-	<div style="display:flex">
+	<div class="optionsContainer">
 	<section>
 		<div id="updateLikeCoinIdStatus"></div>
-		<div>LikeCoin Id: <span id="likecoinId"><?php echo esc_html( $likecoin_id ); ?></span>
-		<a class="changeBtn" style="cursor: pointer">change</a></div>
-		<div>LikeCoin Wallet: <span id="likecoinWallet"><?php echo esc_html( $likecoin_wallet ); ?></span></div>
-		<label for="lc_widget_option">Display Option</label>
-		<select name="lc_widget_option" id="lc_widget_option" class="postbox">
-			<option value="both" 
-			<?php
-			if ( 'both' === $widget_position ) {
-				echo 'selected';
-			}
-			?>
-			>Top and bottom</option>
-			<option value="top" 
-			<?php
-			if ( 'top' === $widget_position ) {
-				echo 'selected';
-			}
-			?>
-			>Top</option>
-			<option value="bottom" 
-			<?php
-			if ( 'bottom' === $widget_position ) {
-				echo 'selected';
-			}
-			?>
-			>Bottom</option>
-			<option value="none" 
-			<?php
-			if ( 'none' === $widget_position ) {
-				echo 'selected';
-			}
-			?>
-			>None</option>
-		</select>
+		<div class="label">LikeCoin ID <a class="changeBtn">change</a>
+			<div class="field" id="likecoinId">
+				<a href="https://like.co/<?php echo esc_attr( $likecoin_id ); ?>">
+					<?php echo esc_html( $likecoin_id ); ?>
+				</a>
+			</div>
+		</div>
+		<div class="label">LikeCoin Wallet
+			<div class="field" id="likecoinWallet"><?php echo esc_html( substr( $likecoin_wallet, 0, 6 ) . '...' . substr( $likecoin_wallet, 38, 4 ) ); ?></div>
+		</div>
+		<div class="label">
+			<label for="lc_widget_option">Display Option</label>
+			<div><select name="lc_widget_option" id="lc_widget_option" class="postbox">
+				<option value="both" 
+				<?php
+				if ( 'both' === $widget_position ) {
+					echo 'selected';
+				}
+				?>
+				>Top and bottom</option>
+				<option value="top" 
+				<?php
+				if ( 'top' === $widget_position ) {
+					echo 'selected';
+				}
+				?>
+				>Top</option>
+				<option value="bottom" 
+				<?php
+				if ( 'bottom' === $widget_position ) {
+					echo 'selected';
+				}
+				?>
+				>Bottom</option>
+				<option value="none" 
+				<?php
+				if ( 'none' === $widget_position ) {
+					echo 'selected';
+				}
+				?>
+				>None</option>
+			</select></div>
+		</div>
 	</section>
-	<section style="flex:1;-webkit-flex:1;-ms-flex:1">
+	<section class="previewSection">
+		<span>This LikeCoin Widget will be shown in your post:</span>
+		<a class="icon" href="https://like.co/in" target="_blank"><?php echo file_get_contents( LC_DIR . 'assets/icon/settings.svg' ); ?></a>
 		<iframe id="likecoinPreview" scrolling="no" frameborder="0"
 		style="pointer-events: none; height: 212px; width: 100%;"
 		src="
@@ -84,6 +95,8 @@ function likecoin_add_meta_box( $post ) {
 	<?php wp_nonce_field( 'lc_save_post', 'lc_metabox_nonce' ); ?>
 </section>
 	<?php
+	wp_register_style( 'lc_metabox', LC_URI . 'assets/css/metabox.css' );
+  wp_enqueue_style( 'lc_metabox' );
 	wp_enqueue_script( 'lc_metabox', LC_URI . 'assets/js/likecoin.es5.js', false, LC_PLUGIN_VERSION, true );
 	wp_localize_script(
 		'lc_metabox',
