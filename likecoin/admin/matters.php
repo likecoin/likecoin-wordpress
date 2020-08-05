f7ccf13ec592f2f54931f6c32f351eac60e19b65
William Chong <me@williamchong.cloud>
2020-08-06 03:49:02 +0800

    🚧 Replace asset url when submitting to matters

--- likecoin/admin/matters.php
+++ likecoin/admin/matters.php
@@ -1,11 +1,34 @@
 <?php
 require_once dirname( __FILE__ ) . '/../includes/class-likecoin-matters-api.php';
 
+function likecoin_replace_matters_attachment_url( $content ) {
+	if ( ! preg_match_all( '/<img[^>]+src=([\'"])(?<src>.+?)\1[^>]*>/i', $content, $matches ) ) {
+		return $content;
+	}
+	foreach ( $matches[0] as $match ) {
+		if ( preg_match( '@src="([0-9a-z:/._-]+)@i', $match, $src_list ) ) {
+			$url = $src_list[1];
+			if ( $url ) {
+				$attachment_id = attachment_url_to_postid( $url );
+				if ( $attachment_id ) {
+					$matters_info = get_post_meta( $attachment_id, LC_MATTERS_INFO, true );
+					if ( isset( $matters_info['url'] ) ) {
+						$content = str_replace( $url, $matters_info['url'], $content );
+					}
+				}
+			}
+		}
+	}
+	return $content;
+}
+
 function likecoin_save_to_matters( $post_id, $post, $update ) {
 	if ( 'draft' !== get_post_status( $post_id ) ) {
 		return;
 	}
+
 	$content          = apply_filters( 'the_content', $post->post_content );
+	$content          = likecoin_replace_matters_attachment_url( $content );
 	$title            = apply_filters( 'the_title', $post->post_title );
 	$api              = LikeCoin_Matters_API::get_instance();
 	$matters_draft_id = get_post_meta( $post_id, LC_MATTERS_DRAFT_ID, true );
