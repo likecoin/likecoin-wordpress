d9065523db5702de677ca86b07cd0be1f94a6c37
William Chong <me@williamchong.cloud>
2021-08-18 23:23:33 +0800

    🔖 Version 2.2.0

--- likecoin/likecoin.php
+++ likecoin/likecoin.php
@@ -13,7 +13,7 @@
  * Plugin Name:  LikeCoin
  * Plugin URI:   https://github.com/likecoin/likecoin-wordpress
  * Description:  Integrate your Liker ID, add LikeCoin Button and decentralized publishing to WordPress.
- * Version:      2.1.2
+ * Version:      2.2.0
  * Author:       LikeCoin Foundation
  * Author URI:   https://like.co/
  * License:      GPLv3
