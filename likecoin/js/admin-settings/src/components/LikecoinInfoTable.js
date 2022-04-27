1a5f6a26579f456987b5092a2c281ec8ad0efaae
William Chong <me@williamchong.cloud>
2022-04-27 18:04:47 +0800

    💄 Fix LikecoinInfoTable break layout in settings

--- likecoin/js/admin-settings/src/components/LikecoinInfoTable.js
+++ likecoin/js/admin-settings/src/components/LikecoinInfoTable.js
@@ -5,127 +5,121 @@ import Text from './Text';
 function LikecoinInfoTable(props) {
   const likerIdRef = useRef();
   return (
-    <div>
-      <form>
-        <tr>
-          <label>
-            {props.isMainSettingPage && (
-              <th className="optionTitle">{__('Site Liker ID', 'likecoin')}</th>
-            )}
-            <td>
-              <table className="form-table likecoinTable">
-                <tbody>
-                  <tr>
-                    <th>{__('Liker ID', 'likecoin')}</th>
-                    <th>{__('Display Name', 'likecoin')}</th>
-                    <th>{__('Wallet', 'likecoin')}</th>
-                    <th> </th>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="avatarWrapper">
-                        {!props.isLoading
-                          && props.likerAvatar.length > 0
-                          && props.likerAvatar !== '-' && (
-                            <img
-                              id="likecoinAvatar"
-                              className="likecoinAvatar"
-                              src={props.likerAvatar}
-                              alt="Avatar"
-                            />
-                        )}
-                        {props.likerIdValue.length > 0 && props.likerIdValue !== '-'
-                          && !props.isChangingTypingLiker && (
-                            <a
-                              id="likecoinId"
-                              rel="noopener noreferrer"
-                              target="_blank"
-                              href={`https://like.co/${props.likerIdValue}`}
-                              className="likecoin likecoinId"
-                            >
-                              {props.likerIdValue}
-                            </a>
-                        )}
-                        {(props.likerIdValue.length === 0 || props.likerIdValue === '-'
-                          || props.isChangingTypingLiker) && (
-                          <div>
-                            <input
-                              type="text"
-                              id="likecoinIdInputBox"
-                              ref={likerIdRef}
-                              className="likecoinInputBox"
-                              onChange={props.handleLikerIdInputChange}
-                            />
-                            <p>
-                              <a
-                                id="likecoinInputLabel"
-                                className="likecoinInputLabel"
-                                target="blacnk"
-                                rel="noopener"
-                                href="https://like.co/in"
-                              >
-                                {__('Sign Up / Find my Liker ID', 'likecoin')}
-                              </a>
-                            </p>
-                          </div>
-                        )}
-                      </div>
-                    </td>
-                    <td>
-                      {!props.isLoading && (
-                        <Text text={props.likerDisplayName || '-'} />
-                      )}
-                    </td>
-                    <td>
-                      {!props.isLoading && (
-                        <Text text={props.likerWalletAddress || '-'} />
-                      )}
-                    </td>
-                    <td className="actions">
-                      {props.showChangeButton && props.editable && (
-                        <span className="actionWrapper">
-                          <a
-                            id="likecoinChangeBtn"
-                            type="button"
-                            onClick={props.handleClickOnChange}
-                          >
-                            {__('Change', 'likecoin')}
-                          </a>
-                        </span>
-                      )}
-                      {props.showDisconnectButton && props.editable && (
-                        <span className="actionWrapper">
-                          <a
-                            id="likecoinLogoutBtn"
-                            type="button"
-                            onClick={props.handleDisconnect}
-                          >
-                            {__('Disconnect', 'likecoin')}
-                          </a>
-                        </span>
-                      )}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <section className="likecoin loading">
-                {props.likerIdValue.length !== 0
-                  && props.isLoading
-                  && __('Loading...', 'likecoin')}
-              </section>
-
-              <section>
-                {props.likerAvatar === '-' && !props.isLoading && (
-                  <div className="likecoin likecoinError userNotFound">
-                    <h4>{__('Liker ID not found', 'likecoin')}</h4>
-                  </div>
+    <tr>
+      {props.isMainSettingPage && (
+        <th className="optionTitle">{__('Site Liker ID', 'likecoin')}</th>
+      )}
+      <td>
+        <table className="form-table likecoinTable">
+          <tbody>
+            <tr>
+              <th>{__('Liker ID', 'likecoin')}</th>
+              <th>{__('Display Name', 'likecoin')}</th>
+              <th>{__('Wallet', 'likecoin')}</th>
+              <th> </th>
+            </tr>
+            <tr>
+              <td>
+                <div className="avatarWrapper">
+                  {!props.isLoading
+                    && props.likerAvatar.length > 0
+                    && props.likerAvatar !== '-' && (
+                      <img
+                        id="likecoinAvatar"
+                        className="likecoinAvatar"
+                        src={props.likerAvatar}
+                        alt="Avatar"
+                      />
+                  )}
+                  {props.likerIdValue.length > 0 && props.likerIdValue !== '-'
+                    && !props.isChangingTypingLiker && (
+                      <a
+                        id="likecoinId"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href={`https://like.co/${props.likerIdValue}`}
+                        className="likecoin likecoinId"
+                      >
+                        {props.likerIdValue}
+                      </a>
+                  )}
+                  {(props.likerIdValue.length === 0 || props.likerIdValue === '-'
+                    || props.isChangingTypingLiker) && (
+                    <div>
+                      <input
+                        type="text"
+                        id="likecoinIdInputBox"
+                        ref={likerIdRef}
+                        className="likecoinInputBox"
+                        onChange={props.handleLikerIdInputChange}
+                      />
+                      <p>
+                        <a
+                          id="likecoinInputLabel"
+                          className="likecoinInputLabel"
+                          target="blacnk"
+                          rel="noopener"
+                          href="https://like.co/in"
+                        >
+                          {__('Sign Up / Find my Liker ID', 'likecoin')}
+                        </a>
+                      </p>
+                    </div>
+                  )}
+                </div>
+              </td>
+              <td>
+                {!props.isLoading && (
+                  <Text text={props.likerDisplayName || '-'} />
                 )}
-              </section>
-            </td>
-          </label>
-        </tr>
-      </form>
-    </div>
+              </td>
+              <td>
+                {!props.isLoading && (
+                  <Text text={props.likerWalletAddress || '-'} />
+                )}
+              </td>
+              <td className="actions">
+                {props.showChangeButton && props.editable && (
+                  <span className="actionWrapper">
+                    <a
+                      id="likecoinChangeBtn"
+                      type="button"
+                      onClick={props.handleClickOnChange}
+                    >
+                      {__('Change', 'likecoin')}
+                    </a>
+                  </span>
+                )}
+                {props.showDisconnectButton && props.editable && (
+                  <span className="actionWrapper">
+                    <a
+                      id="likecoinLogoutBtn"
+                      type="button"
+                      onClick={props.handleDisconnect}
+                    >
+                      {__('Disconnect', 'likecoin')}
+                    </a>
+                  </span>
+                )}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <section className="likecoin loading">
+          {props.likerIdValue.length !== 0
+            && props.isLoading
+            && __('Loading...', 'likecoin')}
+        </section>
+
+        <section>
+          {props.likerAvatar === '-' && !props.isLoading && (
+            <div className="likecoin likecoinError userNotFound">
+              <h4>{__('Liker ID not found', 'likecoin')}</h4>
+            </div>
+          )}
+        </section>
+      </td>
+    </tr>
   );
 }
 
