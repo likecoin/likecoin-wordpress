29a5e015eb142b4b19860284d1a1f8afc72837eb
William Chong <me@williamchong.cloud>
2022-07-28 23:17:24 +0800

    ✨ Add minting flag for iscn-ar

--- likecoin/js/sidebar/src/pages/LikeCoinPlugin.js
+++ likecoin/js/sidebar/src/pages/LikeCoinPlugin.js
@@ -123,7 +123,7 @@ function LikeCoinPlugin(props) {
   const openISCNWidget = useCallback(() => {
     const iscnId = encodeURIComponent(ISCNId || '');
     const redirectString = encodeURIComponent(siteurl);
-    const popUpWidget = `${ISCN_WIDGET_ORIGIN}/in/widget/iscn-ar?opener=1&redirect_uri=${redirectString}&iscn_id=${iscnId}`;
+    const popUpWidget = `${ISCN_WIDGET_ORIGIN}/in/widget/iscn-ar?opener=1&mint=1&redirect_uri=${redirectString}&iscn_id=${iscnId}`;
     try {
       const popUp = window.open(
         popUpWidget,
