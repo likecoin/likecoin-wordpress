c33ee9da82d0c9b19bb18a5c073faa28cc2f1080
William Chong <me@williamchong.cloud>
2022-12-05 16:28:36 +0800

    🚚 Rename likecoin_metabox to metabox

--- likecoin/assets/js/admin-settings/index.asset.php
+++ likecoin/assets/js/admin-settings/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n' ),
-	'version'      => 'ef77c3c2ea0a0c3dac0d14a4871d85ec',
+	'version'      => '0fb12fc30ea0a2cbb0127a6529a7e3e9',
 );
