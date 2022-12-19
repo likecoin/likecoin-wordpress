6037bebb1b391851cda8976accd90af6ee88c787
William Chong <me@williamchong.cloud>
2022-12-20 00:58:51 +0800

    ✨ Add likecoin meta tag and default style in iscn post html

--- likecoin/admin/post.php
+++ likecoin/admin/post.php
@@ -23,6 +23,147 @@
 // phpcs:disable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 
 /**
+ * Require files
+ */
+require_once dirname( __FILE__ ) . '/../public/likecoin-button.php';
+
+/**
+ * Get default style defined in matters library.
+ * Refer to https://github.com/thematters/matters-html-formatter/blob/main/src/makeHtmlBundle/formatHTML/articleTemplate.ts for details.
+ */
+function likecoin_get_default_post_style() {
+	return '<style>
+	html, body {
+	  margin: 0;
+	  padding: 0;
+	}
+	body {
+	  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif;
+	  font-size: 18px;
+	  line-height: 1.5;
+	}
+	main {
+	  max-width: 673px;
+	  margin: 40px auto;
+	  padding: 0 20px;
+	}
+	hr { height: 1px; }
+	h1, h2, h3, h4, h5, h6 { font-weight: 600; line-height: 1.4; }
+	h1 { font-size: 28px; }
+	h2 { font-size: 24px; }
+	h3 { font-size: 22px; }
+	h4 { font-size: 18px; }
+	h5 { font-size: 16px; }
+	h6 { font-size: 14px; }
+	li ul, li ol { margin: 0 20px; }
+	li { margin: 20px 0; }
+	ul { list-style-type: disc; }
+	ol { list-style-type: decimal; }
+	ol ol { list-style: upper-alpha; }
+	ol ol ol { list-style: lower-roman; }
+	ol ol ol ol { list-style: lower-alpha; }
+	img, video, audio {
+	  display: block;
+	  max-width: 100%;
+	  margin: 0 auto;
+	}
+	audio {
+	  width: 100%;
+	}
+	blockquote {
+	  margin-left: 20px;
+	  margin-right: 20px;
+	  color: #5F5F5F;
+	}
+
+	pre {
+	  white-space: pre-wrap;
+	}
+
+	header {
+	  margin-bottom: 40px;
+	}
+	header h1 {
+	  font-size: 32px;
+	}
+
+
+	figure {
+	  margin: 0;
+	}
+
+	figure.byline {
+	  font-size: 16px;
+	  margin: 0;
+	}
+	figure.byline * + * {
+	  padding-left: 10px;
+	}
+	figure.byline time {
+	  color: #b3b3b3;
+	}
+	figure.byline [ref="source"]::before {
+	  content: "";
+	  border-left: 1px solid currentColor;
+	  padding-left: 10px;
+	}
+
+	figure.summary {
+	  margin: 40px 0;
+	  color: #808080;
+	  font-size: 18px;
+	  font-weight: 500;
+	  line-height: 32px;
+	}
+
+	figure.read_more {
+	  margin: 40px 0;
+	}
+
+	article {
+	  position: relative;
+	}
+
+	article > * {
+	  margin-top: 20px;
+	  margin-bottom: 24px;
+	}
+	article a {
+	  border-bottom: 1px solid currentcolor;
+	  text-decoration: none;
+	  padding-bottom: 2px;
+	}
+	article p {
+	  line-height: 1.8;
+	}
+	figure figcaption {
+	  margin-top: 5px;
+	  font-size: 16px;
+	  color: #b3b3b3;
+	}
+
+	figure .iframe-container {
+	  position: relative;
+	  width: 100%;
+	  height: 0;
+	  padding-top: 56.25%;
+	}
+	figure .iframe-container iframe {
+	  position: absolute;
+	  top: 0;
+	  width: 100%;
+	  height: 100%;
+	}
+
+	.encrypted {
+	  display: flex;
+	  justify-content: center;
+	  word-break: break-all;
+	}
+  </style>';
+}
+
+/**
  * Get all tags names in a post
  *
  * @param WP_Post| $post Post object.
@@ -46,27 +187,42 @@ function likecoin_get_post_tags( $post ) {
  * @param object| $post WordPress post object.
  */
 function likecoin_format_post_to_json_data( $post ) {
-	$files            = array();
-	$title            = apply_filters( 'the_title_rss', $post->post_title );
-	$description      = get_the_excerpt( $post );
-	$feature          = likecoin_get_post_thumbnail_with_relative_image_url( $post );
-	$feature_img_div  = $feature['content'];
-	$feature_img_data = $feature['image'];
-	$relative         = likecoin_get_post_content_with_relative_image_url( $post );
-	$content          = $relative['content'];
-	$image_data       = $relative['images'];
-	$content          = '<!DOCTYPE html><html>
-  	<head> <title>' . $title . '</title>' .
-		'<meta charset="utf-8" />
-		 <meta name="viewport" content="width=device-width, initial-scale=1" />' . '
-		 <meta property="og:title" content="' . $title . '">
-		 <meta name="description" content="' . $description . '" />
-		 <meta property="og:description" content="' . $description . '">
-	</head>
-	<body><header><h1>' . $title . '</h1>' . $feature_img_div . '</header>' . $content . '
-	</body></html>';
-	$file_mime_type   = 'text/html';
-	$filename         = 'index.html';
+	$files             = array();
+	$title             = apply_filters( 'the_title_rss', $post->post_title );
+	$description       = get_the_excerpt( $post );
+	$feature           = likecoin_get_post_thumbnail_with_relative_image_url( $post );
+	$feature_img_div   = $feature['content'];
+	$feature_img_data  = $feature['image'];
+	$relative          = likecoin_get_post_content_with_relative_image_url( $post );
+	$content           = $relative['content'];
+	$image_data        = $relative['images'];
+	$meta_tags         = '<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />' . '
+<meta property="og:title" content="' . $title . '">
+<meta name="description" content="' . $description . '" />
+<meta property="og:description" content="' . $description . '">';
+	$iscn_mainnet_info = get_post_meta( $post->ID, LC_ISCN_INFO, true );
+	$iscn_id           = '';
+	$likecoin_user     = likecoin_get_post_liker_id( $post );
+	if ( $iscn_mainnet_info ) {
+		$iscn_id = $iscn_mainnet_info['iscn_id'];
+	}
+	if ( ! empty( $iscn_id ) ) {
+		$meta_tags = $meta_tags . '<meta name="likecoin:iscn" content="' . esc_attr( $iscn_id ) . '">';
+	}
+	$likecoin_user = likecoin_get_post_liker_id( $post );
+	if ( ! empty( $likecoin_user['id'] ) ) {
+		$meta_tags = $meta_tags . '<meta name="likecoin:liker-id" content="' . esc_attr( $likecoin_user['id'] ) . '">';
+	}
+	if ( ! empty( $likecoin_user['wallet'] ) ) {
+		$meta_tags = $meta_tags . '<meta name="likecoin:wallet" content="' . esc_attr( $likecoin_user['wallet'] ) . '">';
+	}
+	$content        = '<!DOCTYPE html><html>
+<head> <title>' . $title . '</title>' . $meta_tags . likecoin_get_default_post_style() . '</head>
+<body><header><h1>' . $title . '</h1>' . $feature_img_div . '</header>' . $content . '
+</body></html>';
+	$file_mime_type = 'text/html';
+	$filename       = 'index.html';
 	// phpcs:disable WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
 	$files[] = array(
 		'filename' => $filename,
