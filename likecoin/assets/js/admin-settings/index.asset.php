975992a9c9e444cbd5e1206f6e719fc39bbce27a
William Chong <me@williamchong.cloud>
2022-12-22 17:09:26 +0800

    📦️ Update compiled js

--- likecoin/assets/js/admin-settings/index.asset.php
+++ likecoin/assets/js/admin-settings/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n' ),
-	'version'      => '58c918942dda0f4ae19ab878ec0f8eb6',
+	'version'      => 'a821a659a589695530ed9e89fd51299a',
 );
