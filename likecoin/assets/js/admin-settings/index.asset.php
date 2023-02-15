9feb2f6e1d8f26690a84bd555350901c80dc62e4
William Chong <me@williamchong.cloud>
2023-02-15 13:07:25 +0800

    💬 Update about page text

--- likecoin/assets/js/admin-settings/index.asset.php
+++ likecoin/assets/js/admin-settings/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n' ),
-	'version'      => '2ee33a385a19e92a341a64b3bb40df6c',
+	'version'      => 'af3a8fad01348067555b5ee744f24dee',
 );
