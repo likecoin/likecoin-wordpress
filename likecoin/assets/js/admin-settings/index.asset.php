cce5eb382ae79cabec32d4202d198e39bede9a30
William Chong <me@williamchong.cloud>
2023-02-16 18:12:34 +0800

    📦️ Update compiled js

--- likecoin/assets/js/admin-settings/index.asset.php
+++ likecoin/assets/js/admin-settings/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n' ),
-	'version'      => 'dd7c4ae8682192f51e40cb6cfd956965',
+	'version'      => '6bf863d6e994787db7262b32dbc374b9',
 );
--- likecoin/assets/js/sidebar/index.asset.php
+++ likecoin/assets/js/sidebar/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react', 'wp-api-fetch', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-wordcount' ),
-	'version'      => '68b5d8850ee7f8e204a1c4bf63ce1ac1',
+	'version'      => 'cdcb2fbacc74ecfac97e2a83ca81cecd',
 );
