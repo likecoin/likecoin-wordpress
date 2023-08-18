5cd900af6fdb68c9d8af0d34d1bc32d6b4ed0cfe
William Chong <me@williamchong.cloud>
2023-08-18 20:39:16 +0800

    👽️ Rename wpApiSettings to likecoinApiSettings to avoid name clash

--- likecoin/assets/js/sidebar/index.asset.php
+++ likecoin/assets/js/sidebar/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-wordcount' ),
-	'version'      => 'b67094d4d96b1230990a',
+	'version'      => 'fc2ec40825e2401de656',
 );
